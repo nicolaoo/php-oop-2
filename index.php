2d900dedef308460cf3d9dbc4489c80c0bb1d9c3
nicola00 <nicolajobs00@gmail.com>
2023-04-03 15:21:14 +0200

    FEAT restart

--- index.php
+++ index.php
@@ -6,9 +6,6 @@
     require_once __DIR__."\model\pesci.php";
     require_once __DIR__."\model\gatti.php";
 
-    $prodotto1 = new prodotti("cotone", 15, true);
-    var_dump($prodotto1);
-    
 
     $categoria1 = new categorie( "cani", "", "" );
     $categoria2 = new categorie( "", "gatti", "" );
@@ -38,26 +35,29 @@
     
 
 
-    $proddoti1= [
+    $prodotti1= [
+        new prodotti("cotone", "18", true),
         $categoria1,
         $animale1
     ];
 
-    $proddoti2= [
+    $prodotti2= [
+        new prodotti("seta", "94", true),
         $categoria2,
         $animale2
     ];
 
-    $proddoti3 = [
+    $prodotti3 = [
+        new prodotti("acquario", "25", true),
         $categoria3,
         $animale3
    
     ];
     
     
-    var_dump($proddoti1);
-    var_dump($proddoti2);
-    var_dump($proddoti3);
+    var_dump($prodotti1);
+    var_dump($prodotti2);
+    var_dump($prodotti3);
 ?>
 
 <!DOCTYPE html>
@@ -69,20 +69,31 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <title>Boolanimal</title>
+    <title>Boolcommerce</title>
 </head>
 <body>
     <section>
         <div class="container">
             <div class="grid">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                <?php foreach($prodotti1 as $prodotto ) :
+                    
+                    ?>
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Card title:
+                                <?php echo $prodotto->cani ?>
+                            </h5>
+                            <p class="card-text">
+                            <?php echo $prodotto->categorie ?>
+                            </p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
-                </div>
+                    <?php
+                endforeach ?>
+                
             </div>
         </div>
     </section>
